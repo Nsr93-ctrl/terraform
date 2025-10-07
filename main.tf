1898c3e3559164214f6a45912ee3b145f60174d2
Nsr93-ctrl <meridjanasser@gmail.com>
2025-10-07 17:08:04 +0200

    Exercice 3.1 – Structure du projet Terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 terraform {
   required_providers {
     local = {
@@ -14,3 +15,19 @@ resource "local_file" "hello" {
   content  = "Hello Terraform!"
   filename = "${path.module}/hello.txt"
 }
+=======
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx_server"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 80
+    external = var.nginx_port
+  }
+}
+>>>>>>> b6e7e83 (Exercice 3.1 – Structure du projet Terraform)
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "nginx_port" {
+  description = "Port externe pour le conteneur Nginx"
+  type        = number
+  default     = 8080
+}
